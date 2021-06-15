fb4ef7f4f69f0ff5ed90c8e16baf0669a61c1a9a
Rouddy <rouddy@naver.com>
2021-06-15 11:47:58 +0900

    null safety 반영

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -47,8 +47,8 @@ class _MyAppState extends State<MyApp> {
 
   String _send = "";
   String _receive = "";
-  StreamSubscription _subscription;
-  SocketConnection _socketConnection;
+  StreamSubscription? _subscription;
+  SocketConnection? _socketConnection;
   String _result = "";
 
   @override
@@ -57,8 +57,8 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
     SharedPreferences.getInstance().then((sp) {
       setState(() {
-        _serverController.text = sp.getString(KEY_SERVER);
-        _portController.text = sp.getInt(KEY_PORT)?.toString() ?? -1;
+        _serverController.text = sp.getString(KEY_SERVER) ?? "";
+        _portController.text = sp.getInt(KEY_PORT)?.toString() ?? "-1";
       });
     });
   }
@@ -86,7 +86,7 @@ class _MyAppState extends State<MyApp> {
   Future<void> connect() async {
     if (_connected) {
       print("disconnect");
-      _subscription.cancel();
+      _subscription?.cancel();
       setState(() {
         _subscription = null;
       });
