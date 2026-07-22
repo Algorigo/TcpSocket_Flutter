c035f4f6ed30bac8ae5cf31c9e287bdc827056c8
Rouddy <rouddy@naver.com>
2026-07-22 10:55:05 +0900

    plugin -> package gradle, library 버전 업데이트 cocoapods -> spm 으로 변경

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,15 +8,16 @@ import 'package:rxdart/rxdart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:tcpsocket_plugin/socket_connection.dart';
-import 'package:tcpsocket_plugin/tcpsocket_plugin.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
@@ -25,7 +26,6 @@ class _MyAppState extends State<MyApp> {
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
-  String _platformVersion = 'Unknown';
   final _serverController = TextEditingController();
   final _portController = TextEditingController();
 
@@ -54,32 +54,11 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    initPlatformState();
     SharedPreferences.getInstance().then((sp) {
       setState(() {
         _serverController.text = sp.getString(keyServer) ?? "";
         _portController.text = sp.getInt(keyPort)?.toString() ?? "-1";
       });
-    });
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await TcpsocketPlugin.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
     });
   }
 
@@ -153,7 +132,6 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: Column(
             children: [
-              Text('Running on: $_platformVersion\n'),
               TextField(
                 controller: _serverController,
                 keyboardType: TextInputType.text,
--- ios/Classes/TcpsocketPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface TcpsocketPlugin : NSObject<FlutterPlugin>
-@end
